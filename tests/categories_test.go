ea5106fd1384d5c8a8083c1cc39fb6765e130b1a
IndominusByte <nyomanpradipta120@gmail.com>
2022-02-03 22:08:17 +0800

    complete testing

--- tests/categories_test.go
+++ tests/categories_test.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strconv"
+	"strings"
 	"testing"
 
 	categoriesentity "github.com/IndominusByte/learn-go-restful-api/internal/entity/categories"
@@ -136,7 +138,6 @@ func TestCreateCategory(t *testing.T) {
 	repo, s := setupEnvironment()
 
 	var data map[string]interface{}
-	var form map[string]string
 
 	// login
 	req, _ := http.NewRequest(http.MethodGet, "/token/get-token", nil)
@@ -151,24 +152,32 @@ func TestCreateCategory(t *testing.T) {
 	tests := [...]struct {
 		name       string
 		expected   string
+		form       map[string]string
 		statusCode int
 	}{
 		{
 			name:       "success",
 			expected:   "Successfully create category.",
+			form:       map[string]string{"icon": "@/app/static/test_image/image.jpeg", "name": name},
+			statusCode: 201,
+		},
+		{
+			name:       "create another",
+			expected:   "Successfully create category.",
+			form:       map[string]string{"icon": "@/app/static/test_image/image.jpeg", "name": name + "2"},
 			statusCode: 201,
 		},
 		{
 			name:       "duplicate name",
 			expected:   "The name has already been taken.",
+			form:       map[string]string{"icon": "@/app/static/test_image/image.jpeg", "name": name},
 			statusCode: 400,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			form = map[string]string{"icon": "@/app/static/test_image/image.jpeg", "name": name}
-			ct, b, err := createForm(form)
+			ct, b, err := createForm(test.form)
 			if err != nil {
 				panic(err)
 			}
@@ -214,30 +223,30 @@ func TestValidationGetAllCategories(t *testing.T) {
 	var data map[string]interface{}
 
 	tests := [...]struct {
-		name   string
-		prefix string
+		name string
+		url  string
 	}{
 		{
-			name:   "empty",
-			prefix: prefix,
+			name: "empty",
+			url:  prefix,
 		},
 		{
-			name:   "required",
-			prefix: prefix + "?page=&per_page=&q=",
+			name: "required",
+			url:  prefix + "?page=&per_page=&q=",
 		},
 		{
-			name:   "type data",
-			prefix: prefix + "?page=a&per_page=a&q=1",
+			name: "type data",
+			url:  prefix + "?page=a&per_page=a&q=1",
 		},
 		{
-			name:   "minimum",
-			prefix: prefix + "?page=-1&per_page=-1&q=",
+			name: "minimum",
+			url:  prefix + "?page=-1&per_page=-1&q=",
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			req, _ := http.NewRequest(http.MethodGet, test.prefix, nil)
+			req, _ := http.NewRequest(http.MethodGet, test.url, nil)
 
 			response := executeRequest(req, s)
 
@@ -274,4 +283,424 @@ func TestGetAllCategories(t *testing.T) {
 
 	assert.NotNil(t, data["results"].(map[string]interface{})["data"])
 	assert.Equal(t, 200, response.Result().StatusCode)
+}
+
+func TestValidationGetCategoryById(t *testing.T) {
+	/*
+	   standar validation
+
+	   - empty []
+	   - required []
+	   - type data [x]
+	   - format regex []
+	   - minimum [x]
+	   - maximum []
+	   - file []
+	*/
+
+	_, s := setupEnvironment()
+
+	var data map[string]interface{}
+
+	tests := [...]struct {
+		name string
+		url  string
+	}{
+		{
+			name: "type data",
+			url:  prefix + "/abc",
+		},
+		{
+			name: "minimum",
+			url:  prefix + "/-1",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			req, _ := http.NewRequest(http.MethodGet, test.url, nil)
+
+			response := executeRequest(req, s)
+
+			body, _ := io.ReadAll(response.Result().Body)
+			json.Unmarshal(body, &data)
+
+			assert.Equal(t, "404 page not found", strings.TrimSuffix(string(body), "\n"))
+			assert.Equal(t, 404, response.Result().StatusCode)
+		})
+	}
+}
+
+func TestGetCategoryById(t *testing.T) {
+	repo, s := setupEnvironment()
+
+	var data map[string]interface{}
+
+	// get id
+	payload := categoriesentity.FormCreateUpdateSchema{Name: name}
+	category, _ := repo.categoriesRepo.GetCategoryByName(context.Background(), &payload)
+
+	tests := [...]struct {
+		name string
+		url  string
+	}{
+		{
+			name: "not found",
+			url:  prefix + "/99999999",
+		},
+		{
+			name: "success",
+			url:  prefix + "/" + strconv.Itoa(category.Id),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			req, _ := http.NewRequest(http.MethodGet, test.url, nil)
+
+			response := executeRequest(req, s)
+
+			body, _ := io.ReadAll(response.Result().Body)
+			json.Unmarshal(body, &data)
+
+			switch test.name {
+			case "not found":
+				assert.Equal(t, "Category not found.", data["detail_message"].(map[string]interface{})["_app"].(string))
+				assert.Equal(t, 404, response.Result().StatusCode)
+			case "success":
+				assert.NotNil(t, data["results"])
+				assert.Equal(t, 200, response.Result().StatusCode)
+			}
+		})
+	}
+}
+
+func TestValidationUpdateCategoryById(t *testing.T) {
+	_, s := setupEnvironment()
+
+	var data map[string]interface{}
+
+	// login
+	req, _ := http.NewRequest(http.MethodGet, "/token/get-token", nil)
+	response := executeRequest(req, s)
+	result := response.Result()
+
+	body, _ := io.ReadAll(result.Body)
+	json.Unmarshal(body, &data)
+
+	accessToken := data["results"].(map[string]interface{})["access_token"].(string)
+
+	/*
+	   standar validation
+
+	   - empty [x]
+	   - required [x]
+	   - type data [x]
+	   - format regex []
+	   - minimum [x]
+	   - maximum [x]
+	   - file [x]
+	*/
+
+	tests := [...]struct {
+		name     string
+		expected string
+		url      string
+		form     map[string]string
+	}{
+		{
+			name:     "empty",
+			expected: "Invalid input type.",
+			url:      prefix + "/1",
+			form:     map[string]string{"icon": "@", "name": ""},
+		},
+		{
+			name:     "optional icon & required name",
+			expected: "Missing data for required field.",
+			url:      prefix + "/1",
+			form:     map[string]string{},
+		},
+		{
+			name:     "type data path",
+			expected: "404 page not found",
+			url:      prefix + "/abc",
+			form:     map[string]string{},
+		},
+		{
+			name:     "minimum path",
+			expected: "404 page not found",
+			url:      prefix + "/-1",
+			form:     map[string]string{},
+		},
+		{
+			name:     "minimum form",
+			expected: "Shorter than minimum length 3.",
+			url:      prefix + "/1",
+			form:     map[string]string{"name": "a"},
+		},
+		{
+			name:     "maximum form",
+			expected: "Longer than maximum length 100.",
+			url:      prefix + "/1",
+			form:     map[string]string{"name": createMaximum(200)},
+		},
+		{
+			name:     "danger file extension",
+			expected: "Image must be between jpeg, png.",
+			url:      prefix + "/1",
+			form:     map[string]string{"icon": "@/app/static/test_image/test.txt", "name": "aaa"},
+		},
+		{
+			name:     "not valid file extension",
+			expected: "Image must be between jpeg, png.",
+			url:      prefix + "/1",
+			form:     map[string]string{"icon": "@/app/static/test_image/test.gif", "name": "aaa"},
+		},
+		{
+			name:     "file cannot grater than 4 Mb",
+			expected: "An image cannot greater than 4 Mb.",
+			url:      prefix + "/1",
+			form:     map[string]string{"icon": "@/app/static/test_image/size.png", "name": "aaa"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ct, b, err := createForm(test.form)
+			if err != nil {
+				panic(err)
+			}
+
+			req, _ = http.NewRequest(http.MethodPut, test.url, b)
+			req.Header.Add("Authorization", "Bearer "+accessToken)
+			req.Header.Set("Content-Type", ct)
+
+			response = executeRequest(req, s)
+
+			body, _ = io.ReadAll(response.Result().Body)
+			json.Unmarshal(body, &data)
+
+			switch test.name {
+			case "empty":
+				assert.Equal(t, test.expected, data["detail_message"].(map[string]interface{})["_body"].(string))
+				assert.Equal(t, 422, response.Result().StatusCode)
+			case "optional icon & required name":
+				assert.Equal(t, test.expected, data["detail_message"].(map[string]interface{})["name"].(string))
+				assert.Nil(t, data["detail_message"].(map[string]interface{})["icon"])
+				assert.Equal(t, 422, response.Result().StatusCode)
+			case "type data path", "minimum path":
+				assert.Equal(t, "404 page not found", strings.TrimSuffix(string(body), "\n"))
+				assert.Equal(t, 404, response.Result().StatusCode)
+			case "minimum form", "maximum form":
+				assert.Equal(t, test.expected, data["detail_message"].(map[string]interface{})["name"].(string))
+				assert.Equal(t, 422, response.Result().StatusCode)
+			case "danger file extension", "not valid file extension", "file cannot grater than 4 Mb":
+				assert.Equal(t, test.expected, data["detail_message"].(map[string]interface{})["icon"].(string))
+				assert.Equal(t, 422, response.Result().StatusCode)
+			}
+		})
+	}
+
+}
+
+func TestUpdateCategoryById(t *testing.T) {
+	repo, s := setupEnvironment()
+
+	var data map[string]interface{}
+
+	// get id
+	payload := categoriesentity.FormCreateUpdateSchema{Name: name}
+	category, _ := repo.categoriesRepo.GetCategoryByName(context.Background(), &payload)
+
+	// login
+	req, _ := http.NewRequest(http.MethodGet, "/token/get-token", nil)
+	response := executeRequest(req, s)
+	result := response.Result()
+
+	body, _ := io.ReadAll(result.Body)
+	json.Unmarshal(body, &data)
+
+	accessToken := data["results"].(map[string]interface{})["access_token"].(string)
+
+	tests := [...]struct {
+		name       string
+		expected   string
+		url        string
+		form       map[string]string
+		statusCode int
+	}{
+		{
+			name:       "not found",
+			expected:   "Category not found.",
+			url:        prefix + "/99999999",
+			form:       map[string]string{"name": name},
+			statusCode: 404,
+		},
+		{
+			name:       "success",
+			expected:   "Successfully update the category.",
+			url:        prefix + "/" + strconv.Itoa(category.Id),
+			form:       map[string]string{"icon": "@/app/static/test_image/image.jpeg", "name": name},
+			statusCode: 200,
+		},
+		{
+			name:       "duplicate name",
+			expected:   "The name has already been taken.",
+			url:        prefix + "/" + strconv.Itoa(category.Id),
+			form:       map[string]string{"icon": "@/app/static/test_image/image.jpeg", "name": name + "2"},
+			statusCode: 400,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ct, b, err := createForm(test.form)
+			if err != nil {
+				panic(err)
+			}
+
+			req, _ = http.NewRequest(http.MethodPut, test.url, b)
+			req.Header.Add("Authorization", "Bearer "+accessToken)
+			req.Header.Set("Content-Type", ct)
+
+			response = executeRequest(req, s)
+
+			body, _ = io.ReadAll(response.Result().Body)
+			json.Unmarshal(body, &data)
+
+			switch test.name {
+			case "not found", "duplicate name":
+				assert.Equal(t, test.expected, data["detail_message"].(map[string]interface{})["_app"].(string))
+			case "success":
+				assert.Equal(t, test.expected, data["detail_message"].(map[string]interface{})["_app"].(string))
+				// check the previous image doesn't exists in directory
+				assert.False(t, fileExists("/app/static/icon-categories/"+category.Icon))
+				// check image exists in directory
+				payload := categoriesentity.FormCreateUpdateSchema{Name: name}
+				r, _ := repo.categoriesRepo.GetCategoryByName(context.Background(), &payload)
+				assert.True(t, fileExists("/app/static/icon-categories/"+r.Icon))
+			}
+
+			assert.Equal(t, test.statusCode, response.Result().StatusCode)
+		})
+	}
+}
+
+func TestValidationDeleteCategoryById(t *testing.T) {
+	/*
+	   standar validation
+
+	   - empty []
+	   - required []
+	   - type data [x]
+	   - format regex []
+	   - minimum [x]
+	   - maximum []
+	   - file []
+	*/
+
+	_, s := setupEnvironment()
+
+	var data map[string]interface{}
+
+	tests := [...]struct {
+		name string
+		url  string
+	}{
+		{
+			name: "type data",
+			url:  prefix + "/abc",
+		},
+		{
+			name: "minimum",
+			url:  prefix + "/-1",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			req, _ := http.NewRequest(http.MethodDelete, test.url, nil)
+
+			response := executeRequest(req, s)
+
+			body, _ := io.ReadAll(response.Result().Body)
+			json.Unmarshal(body, &data)
+
+			assert.Equal(t, "404 page not found", strings.TrimSuffix(string(body), "\n"))
+			assert.Equal(t, 404, response.Result().StatusCode)
+		})
+	}
+}
+
+func TestDeleteCategoryById(t *testing.T) {
+	repo, s := setupEnvironment()
+
+	var data map[string]interface{}
+
+	// get id
+	payload := categoriesentity.FormCreateUpdateSchema{Name: name}
+	category1, _ := repo.categoriesRepo.GetCategoryByName(context.Background(), &payload)
+
+	payload = categoriesentity.FormCreateUpdateSchema{Name: name + "2"}
+	category2, _ := repo.categoriesRepo.GetCategoryByName(context.Background(), &payload)
+
+	// login
+	req, _ := http.NewRequest(http.MethodGet, "/token/get-token", nil)
+	response := executeRequest(req, s)
+	result := response.Result()
+
+	body, _ := io.ReadAll(result.Body)
+	json.Unmarshal(body, &data)
+
+	accessToken := data["results"].(map[string]interface{})["access_token"].(string)
+
+	tests := [...]struct {
+		name       string
+		expected   string
+		url        string
+		statusCode int
+	}{
+		{
+			name:       "not found",
+			expected:   "Category not found.",
+			url:        prefix + "/99999999",
+			statusCode: 404,
+		},
+		{
+			name:       "delete 1",
+			expected:   "Successfully delete the category.",
+			url:        prefix + "/" + strconv.Itoa(category1.Id),
+			statusCode: 200,
+		},
+		{
+			name:       "delete 2",
+			expected:   "Successfully delete the category.",
+			url:        prefix + "/" + strconv.Itoa(category2.Id),
+			statusCode: 200,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			req, _ = http.NewRequest(http.MethodDelete, test.url, nil)
+			req.Header.Add("Authorization", "Bearer "+accessToken)
+
+			response = executeRequest(req, s)
+
+			body, _ = io.ReadAll(response.Result().Body)
+			json.Unmarshal(body, &data)
+
+			assert.Equal(t, test.expected, data["detail_message"].(map[string]interface{})["_app"].(string))
+			assert.Equal(t, test.statusCode, response.Result().StatusCode)
+
+			// check the previous image doesn't exists in directory
+			switch test.name {
+			case "delete 1":
+				assert.False(t, fileExists("/app/static/icon-categories/"+category1.Icon))
+			case "delete 2":
+				assert.False(t, fileExists("/app/static/icon-categories/"+category2.Icon))
+			}
+		})
+	}
+
 }
